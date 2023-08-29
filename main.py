9fda19440d7ed93151cf834edbd0fcf831ec7934
botsheloramela <ramelabotshelo12@gmail.com>
2023-08-29 11:54:35 +0200

    Enhance QR code generator to save codes in a PDF

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import qrcode
 from PIL import Image
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
 import os
 import random
 import string
@@ -7,15 +9,15 @@ import string
 # Constants
 LOGO_SIZE = (80, 80)
 QR_VERSION = 1
-DATA_TO_ENCODE = 'https://github.com/BotsheloRamela/qr-code'
 LOGO_FILE_NAME = 'github-logo.png'
+OUTPUT_PDF = 'qr_codes.pdf'
 
 def generate_item_code():
     """"Generate a random 6-character code"""
     characters = string.ascii_uppercase + string.digits
     return ''.join(random.choice(characters) for _ in range(6))
 
-def generate_qr_code_with_logo(item_id):
+def generate_qr_code_with_logo(item_id, qr_canvas):
     """"Create a QRCode instance with the desired size"""
     qr_code = qrcode.QRCode(
         version=QR_VERSION,
@@ -47,15 +49,18 @@ def generate_qr_code_with_logo(item_id):
         # Paste the logo on the QR code image
         qr_code_image.paste(logo_resized, logo_position)
 
-        # Save the QR code image
-        qr_code_image.save(f'qr_code_{item_id}.png')
-        print(f'QR code generated for Item ID {item_id}!')
+        # Draw the QR code image on the PDF canvas
+        qr_canvas.drawInlineImage(qr_code_image, 100, 500 - (item_id - 1) * 150, width=300, height=300)
 
     except Exception as e:
         print(f'An error occurred: {e}')
 
 if __name__ == "__main__":
+    c = canvas.Canvas(OUTPUT_PDF, pagesize=letter)
     num_items = 10  # Number of QR codes to generate
 
     for item_id in range(1, num_items + 1):
-        generate_qr_code_with_logo(item_id)
+        generate_qr_code_with_logo(item_id, c)
+
+    c.save()
+    print(f'QR codes saved in {OUTPUT_PDF}!')
