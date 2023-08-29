a678846e78238173ca6bacff7792d8886d94c98f
botsheloramela <ramelabotshelo12@gmail.com>
2023-08-29 11:48:00 +0200

    Enhance QR code generator for item inventory tracking

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import qrcode
 from PIL import Image
 import os
+import random
+import string
 
 # Constants
 LOGO_SIZE = (80, 80)
@@ -8,13 +10,24 @@ QR_VERSION = 1
 DATA_TO_ENCODE = 'https://github.com/BotsheloRamela/qr-code'
 LOGO_FILE_NAME = 'github-logo.png'
 
-def generate_qr_code_with_logo():
+def generate_item_code():
+    """"Generate a random 6-character code"""
+    characters = string.ascii_uppercase + string.digits
+    return ''.join(random.choice(characters) for _ in range(6))
+
+def generate_qr_code_with_logo(item_id):
     """"Create a QRCode instance with the desired size"""
     qr_code = qrcode.QRCode(
         version=QR_VERSION,
         error_correction=qrcode.constants.ERROR_CORRECT_H
     )
-    qr_code.add_data(DATA_TO_ENCODE)
+
+    # Generate a unique item code
+    item_code = generate_item_code()
+
+    # Combine item ID and item code for QR code data
+    data_to_encode = f'Item ID: {item_id}\nItem Code: {item_code}'
+    qr_code.add_data(data_to_encode)
     qr_code.make(fit=True)
 
     # Make a QR code image
@@ -35,11 +48,14 @@ def generate_qr_code_with_logo():
         qr_code_image.paste(logo_resized, logo_position)
 
         # Save the QR code image
-        qr_code_image.save('qr_code.png')
-        print('QR code generated!')
+        qr_code_image.save(f'qr_code_{item_id}.png')
+        print(f'QR code generated for Item ID {item_id}!')
 
     except Exception as e:
         print(f'An error occurred: {e}')
 
 if __name__ == "__main__":
-    generate_qr_code_with_logo()
+    num_items = 10  # Number of QR codes to generate
+
+    for item_id in range(1, num_items + 1):
+        generate_qr_code_with_logo(item_id)
